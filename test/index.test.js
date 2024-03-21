d2a04681c66ae54842dbc5dfc08487fbc89fe030
debasish <debasish.sahoo96@gmail.com>
2024-03-21 19:40:55 +0530

    test(js): index.test.js
    Add another test in test .js file
    
    test#1

--- test/index.test.js
+++ test/index.test.js
@@ -7,3 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 5 + 9 to equal 14', () => {
     expect(add(5, 9)).toBe(14);
 });
+
+test('adds 5 + 10 to equal 15', () => {
+    expect(add(5, 10)).toBe(15);
+});
