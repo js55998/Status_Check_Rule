df5c111263116adeb5fb80a95c1d57c56bd8a831
debasish <debasish.sahoo96@gmail.com>
2024-03-21 11:17:50 +0530

    feat(js): index.js
    create index.js in app dir and index.test.js in test dir
    
    feat#2

---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+const add = require('../app/index')
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+});
+
+test('adds 5 + 9 to equal 14', () => {
+    expect(add(5, 9)).toBe(14);
+});
