dd34a139dbf4c91fca8b776d87d165f4f3076411
Debasish Sahoo <debasish.sahoo96@gmail.com>
2024-03-27 17:25:57 +0530

    Feature/calculator (#7)
    * This is Unsigned Verifaction
    
    * This is Reissue

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
 const cal = require("../app/index")
-test('adds 1 + 2 to equal 3', () => {
+test('Adds 1 + 2 to equal 3', () => {
     expect(cal.add(1, 2)).toBe(3);
 });
 test('Sub 5 - 3 to equal 2', () => {
@@ -8,6 +8,9 @@ test('Sub 5 - 3 to equal 2', () => {
 test('Multiply 2*2 to equal 4', () => {
     expect(cal.mul(2, 2)).toBe(4);
 });
-test('div 15 / 3 to equal 5', () => {
+test('Div 15 / 3 to equal 5', () => {
     expect(cal.div(15, 3)).toBe(5);
+});
+test('Mod 15 % 3 to equal 5', () => {
+    expect(cal.mod(15, 3)).toBe(0);
 });
